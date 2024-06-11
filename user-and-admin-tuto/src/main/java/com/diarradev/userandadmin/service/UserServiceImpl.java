4edeccc5c61f869588e32e48c4fc3bb6ffb95f35
Moise DIARRA <moisepr@gmail.com>
2024-06-11 05:07:58 +0000

    Update service

--- user-and-admin-tuto/src/main/java/com/diarradev/userandadmin/service/UserServiceImpl.java
+++ user-and-admin-tuto/src/main/java/com/diarradev/userandadmin/service/UserServiceImpl.java
@@ -1,9 +1,12 @@
 package com.diarradev.userandadmin.service;
 
+import org.springframework.stereotype.Service;
+
 import com.diarradev.userandadmin.dto.UserDto;
 import com.diarradev.userandadmin.model.User;
 import com.diarradev.userandadmin.repositories.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 
 	private UserRepository userRepository;
