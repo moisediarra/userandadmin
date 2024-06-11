a1eb4ce2e80466d858168b3779a29641fa70ec47
Moise DIARRA <moisepr@gmail.com>
2024-06-11 04:31:05 +0000

    Add User service

--- user-and-admin-tuto/src/main/java/com/diarradev/userandadmin/service/UserService.java
+++ user-and-admin-tuto/src/main/java/com/diarradev/userandadmin/service/UserService.java
@@ -1,8 +1,9 @@
 package com.diarradev.userandadmin.service;
 
+import com.diarradev.userandadmin.dto.UserDto;
 import com.diarradev.userandadmin.model.User;
 
 public interface UserService {
 
-	User save();
+	User save(UserDto userDto);
 }
