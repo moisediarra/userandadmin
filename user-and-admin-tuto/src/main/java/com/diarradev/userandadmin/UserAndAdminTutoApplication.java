c03ab46bb4125d4ba9e847bc472dc66cde2adfdf
Moise DIARRA <moisepr@gmail.com>
2024-06-11 03:21:38 +0000

    First commit

---
+++ user-and-admin-tuto/src/main/java/com/diarradev/userandadmin/UserAndAdminTutoApplication.java
@@ -0,0 +1,13 @@
+package com.diarradev.userandadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAndAdminTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAndAdminTutoApplication.class, args);
+	}
+
+}
---
+++ user-and-admin-tuto/src/test/java/com/diarradev/userandadmin/UserAndAdminTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diarradev.userandadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAndAdminTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
