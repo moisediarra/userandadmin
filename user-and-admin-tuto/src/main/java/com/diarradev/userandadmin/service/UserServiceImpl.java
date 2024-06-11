0005bf51f6b3624775fbbc349c1408a92dc38ef5
Moise DIARRA <moisepr@gmail.com>
2024-06-11 04:42:03 +0000

    Update UserServiceImpl

--- user-and-admin-tuto/src/main/java/com/diarradev/userandadmin/service/UserServiceImpl.java
+++ user-and-admin-tuto/src/main/java/com/diarradev/userandadmin/service/UserServiceImpl.java
@@ -1,5 +1,16 @@
 package com.diarradev.userandadmin.service;
 
-public class UserServiceImpl {
+import com.diarradev.userandadmin.dto.UserDto;
+import com.diarradev.userandadmin.model.User;
+import com.diarradev.userandadmin.repositories.UserRepository;
+
+public class UserServiceImpl implements UserService {
+
+	private UserRepository userRepository;
+	@Override
+	public User save(UserDto userDto) {
+		User user = new User(userDto.getEmail(), userDto.getPassword(), userDto.getRole(), userDto.getFullname());
+		return userRepository.save(user);
+	}
 
 }
